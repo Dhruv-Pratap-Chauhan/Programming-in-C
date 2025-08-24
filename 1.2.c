c470eddde23703eb5c5f00cea48c1cdfa1923f16
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-08-24 17:05:54 +0530

    made different lines code

--- 1.2.c
+++ 1.2.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+int main()
+{
+    printf("Its me dhruv\n");
+    printf("trying to print adresses in\n");
+    printf("different lines\n");
+    printf("in c\n");
+    return 0;
+}
