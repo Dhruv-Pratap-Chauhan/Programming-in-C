87a84d8388607116b10b49fa62e8064fdf43280e
Dhruv Pratap Chauhan <dhruvpratapchauhan7@gmail.com>
2025-08-25 11:28:11 +0530

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -2,7 +2,7 @@
 int main()
 {
     printf("Its me dhruv\n");
-    printf("trying to print adresses in\n");
+    printf("trying to print in\n");
     printf("different lines\n");
     printf("in c\n");
     return 0;
