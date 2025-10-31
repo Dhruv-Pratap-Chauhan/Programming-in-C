c3e11d2d8f33fca4a2ec8b58f8ee40704fdaaec6
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-10-31 08:11:07 +0530

    ok

--- Class/8.1.c
+++ Class/8.1.c
@@ -12,7 +12,6 @@ int main() {
     printf("Value of a = %d, Value pointed by p_int = %d\n", a, *p_int);
     printf("Value of b = %.2f, Value pointed by p_float = %.2f\n", b, *p_float);
     printf("Value of c = %c, Value pointed by p_char = %c\n", c, *p_char);
-
     printf("Address stored in p_int = %p\n", (void*)p_int);
     printf("Address stored in p_float = %p\n", (void*)p_float);
     printf("Address stored in p_char = %p\n", (void*)p_char);
