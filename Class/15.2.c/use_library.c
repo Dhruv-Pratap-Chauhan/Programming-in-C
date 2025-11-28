d089923491d0a3980ca223a808fda08161f4f1cc
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-11-28 11:49:12 +0530

    15

---
+++ Class/15.2.c/use_library.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mymath.h"
+
+int main() {
+    int a = 6, b = 4;
+
+    printf("Addition: %d\n", add(a, b));
+    printf("Square of %d: %d\n", a, square(a));
+
+    return 0;
+}
